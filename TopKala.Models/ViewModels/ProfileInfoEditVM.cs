33247b7788eceb1e9b4e686f1a7250862004f0f2
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2021-09-15 22:04:46 +0430

    Fix InfoEdit view error visual

--- TopKala.Models/ViewModels/ProfileInfoEditVM.cs
+++ TopKala.Models/ViewModels/ProfileInfoEditVM.cs
@@ -13,19 +13,19 @@ namespace TopKala.Models.ViewModels
         public string LastName { get; set; }
 
         [Display(Name = "شماره موبایل", Prompt = "شماره موبایل خود را وارد کنید")]
-        [RegularExpression(SD_Regex.NativePhoneNumber)]
+        [RegularExpression(SD_Regex.NativePhoneNumber, ErrorMessage = "لطفا شماره موبایل خود را به درستی وارد نمایید")]
         public string PhoneNumber { get; set; }
 
         [Display(Name = "کد ملی", Prompt = "کد ملی خود را وارد کنید")]
         public string IdNumber { get; set; }
 
         [Display(Name = "شماره کارت", Prompt = "شماره کارت خود را وارد کنید")]
-        [StringLength(16, MinimumLength = 16)]
+        [StringLength(16, MinimumLength = 16, ErrorMessage = "اعداد شماره کارت وارد شده باید ۱۶ رقم باشد")]
         public string CardNumber { get; set; }
         
         [Display(Name = "آدرس ایمیل", Prompt = "آدرس ایمیل خود را وارد کنید")]
-        [EmailAddress]
-        [Required]
+        [EmailAddress(ErrorMessage = "لطفا آدرس ایمیل خود را به درستی وارد نمایید")]
+        [Required(ErrorMessage = "ایمیل نمی تواند خالی باشد")]
         public string Email { get; set; }
 
         [DisplayName("تبعه خارجی فاقد کد ملی هستم")]
